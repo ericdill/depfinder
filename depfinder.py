c578d26c614f032056640b44432d21c396dc4b56
Eric Dill <edill@bnl.gov>
2015-10-03 13:45:53 -0400

    ENH: Remove config dict from depfinder
    Instead, explicitly sort all imports into one of:
    - 'required'
    - 'questionable'
    - 'builtin'
    - 'relative'
    
    where a module can appear in both 'builtin' and
    'required'/'questionable'

--- depfinder.py
+++ depfinder.py
@@ -20,29 +20,47 @@ from __future__ import print_function, division, absolute_import
 import ast
 import os
 from collections import deque, defaultdict
-import sys
 from stdlib_list import stdlib_list
 
 __version__ = 'v0.0.0'
 
-
-conf = {
-    'include_relative_imports': False,
-    'ignore_builtin_modules': True,
-    'pyver': None,
-}
-
+import sys
+pyver = '%s.%s' % (sys.version_info.major, sys.version_info.minor)
+builtin_modules = stdlib_list(pyver)
+del pyver
+del sys
 
 class ImportCatcher(ast.NodeVisitor):
-    def __init__(self, include_relative_imports=False):
-        self.include_relative_imports = include_relative_imports
-        self.required_modules = deque()
-        self.sketchy_modules = deque()
+    """Find all imports in an Abstract Syntax Tree (AST).
+
+    Attributes
+    ----------
+    required_modules : deque
+        The list of imports that were found outside of try/except blocks
+    sketchy_modules : deque
+        The list of imports that were found inside of try/except blocks
+    self.imports : deque
+    include_relative_imports : bool
+    """
+    def __init__(self):
+        self.required_modules = set()
+        self.sketchy_modules = set()
+        self.builtin_modules = set()
+        self.relative_modules = set()
         self.imports = deque()
         self.import_froms = deque()
         self.trys = {}
 
     def generic_visit(self, node):
+        """Called if no explicit visitor function exists for a node.
+
+        Overridden from the ast.NodeVisitor base class so that I can add some
+        local state to keep track of whether or not my node visitor is inside
+        a try/except block.  When a try block is encountered, the node is added
+        to the `trys` instance attribute and then the try block is recursed in
+        to.  Once the recursion has exited, the node is removed from the `trys`
+        instance attribute
+        """
         for field, value in ast.iter_fields(node):
             if isinstance(value, list):
                 for item in value:
@@ -59,41 +77,106 @@ class ImportCatcher(ast.NodeVisitor):
                 self.visit(value)
 
     def visit_Import(self, node):
+        """Executes when an ast.Import node is encountered
+
+        an ast.Import node is something like 'import bar'
+
+        If ImportCatcher is inside of a try block then the import that has just
+        been encountered will be added to the `sketchy_modules` instance
+        attribute. Otherwise the module will be added to the `required_modules`
+        instance attribute
+        """
         self.imports.append(node)
-        mods = [name.name.split('.')[0] for name in node.names]
-        if self.trys:
-            self.sketchy_modules.extend(mods)
-        else:
-            self.required_modules.extend(mods)
+        mods = set([name.name.split('.')[0] for name in node.names])
+        for mod in mods:
+            self._add_import_node(mod)
 
     def visit_ImportFrom(self, node):
+        """Executes when an ast.ImportFrom node is encountered
+
+        an ast.ImportFrom node is something like 'from foo import bar'
+
+        If ImportCatcher is inside of a try block then the import that has just
+        been encountered will be added to the `sketchy_modules` instance
+        attribute. Otherwise the module will be added to the `required_modules`
+        instance attribute
+        """
         self.import_froms.append(node)
         if node.module is None:
             # this is a relative import like 'from . import bar'
             # so do nothing
             return
-        elif not self.include_relative_imports and node.level == 0:
-            # this is a non-relative import like 'from foo import bar'
-            mod = node.module.split('.')[0]
-        elif self.include_relative_imports and node.level > 0:
+        if node.level > 0:
             # this is a relative import like 'from .foo import bar'
-            mod = node.module.split('.')[0]
-        else:
+            node_name = node.module.split('.')[0]
+            self.relative_modules.add(node_name)
+            return
+        # this is a non-relative import like 'from foo import bar'
+        node_name = node.module.split('.')[0]
+        self._add_import_node(node_name)
+
+    def _add_import_node(self, node_name):
+        # see if we are in a try block
+        if self.trys:
+            self.sketchy_modules.add(node_name)
             return
 
-        if self.trys:
-            self.sketchy_modules.append(mod)
-        else:
-            self.required_modules.append(mod)
+        # see if the module is a builtin
+        if node_name in builtin_modules:
+            self.builtin_modules.add(node_name)
+            return
+        # if none of the above cases are true, it is likely that this
+        # ImportFrom node occurs at the top level of the module
+        self.required_modules.add(node_name)
 
+    def describe(self):
+        """Return the found imports
+
+        Returns
+        -------
+        dict :
+            'required': The modules that were encountered outside of a
+                        try/except block
+            'questionable': The modules that were encountered inside of a
+                            try/except block
+            'relative': The modules that were imported via relative import
+                        syntax
+            'builtin' : The modules that are part of the standard library
+        """
+        return {
+            'required': self.required_modules,
+            'relative': self.relative_modules,
+            'questionable': self.sketchy_modules,
+            'builtin': self.builtin_modules
+        }
 
 
 def get_imported_libs(code):
+    """Given a code snippet, return a list of the imported libraries
+
+    Parameters
+    ----------
+    code : str
+        The code to parse and look for imports
+
+    Returns
+    -------
+    ImportCatcher
+
+    Examples
+    --------
+    >>> import depfinder
+    >>> depfinder.get_imported_libs('from foo import bar')
+    {'required': {'foo'}, 'questionable': set()}
+    >>> with open('depfinder.py') as f:
+            code = f.read()
+            imports = depfinder.get_imported_libs(code)
+            print(imports.describe())
+    """
     tree = ast.parse(code)
-    catcher = ImportCatcher(include_relative_imports=conf['include_relative_imports'])
+    catcher = ImportCatcher()
     catcher.visit(tree)
-    return {'required': set(catcher.required_modules),
-            'questionable': set(catcher.sketchy_modules)}
+    return catcher
 
 
 def iterate_over_library(path_to_source_code):
@@ -109,12 +192,5 @@ def iterate_over_library(path_to_source_code):
                     code = f.read()
                 for k, v in get_imported_libs(code).items():
                     libs[k].update(v)
-
-    if conf['ignore_builtin_modules']:
-        if not conf['pyver']:
-            pyver = '%s.%s' % (sys.version_info.major, sys.version_info.minor)
-        std_libs = stdlib_list("3.4")
-        # print(std_libs)
-        libs['required'] = [lib for lib in libs['required'] if lib not in std_libs]
     libs = {k: sorted(list(v)) for k, v in libs.items()}
     return libs
