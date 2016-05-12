fe732e4ea5c0c7d54a85c71e90703c1410fdfa74
Eric Dill <edill@bnl.gov>
2016-05-12 18:09:23 -0400

    Use requirements.txt and update setup.py accordingly

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
-VERSION = '1.1.1'
+
+VERSION = '1.1.2'
+
+required = open('requirements.txt').read().split('\n')
 
 setup(name='depfinder',
       version=VERSION,
@@ -9,7 +12,7 @@ setup(name='depfinder',
       description='Find all the imports in your library',
       url='http://github.com/ericdill/depfinder',
       platforms='Cross platform (Linux, Mac OSX, Windows)',
-      install_requires=['stdlib_list', 'setuptools'],
+      install_requires=required,
       license='GPLv3',
       entry_points = {"console_scripts": ['depfinder = depfinder:cli']},
 )
