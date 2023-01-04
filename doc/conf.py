cd942bf6f9161f036efc7fa3548d686df58775c5
Filipe Fernandes <ocefpaf@gmail.com>
2023-01-04 20:29:42 -0300

    re-enable myst

--- doc/conf.py
+++ doc/conf.py
@@ -31,6 +31,7 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
@@ -39,8 +40,9 @@ extensions = [
     'sphinx.ext.extlinks',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
-    # 'myst_parser'
 ]
+
+
 # map to the standard library
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None)}
 numpydoc_show_class_members = False
@@ -49,12 +51,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = {
-    '.rst': 'restructuredtext',
-    # '.txt': 'markdown',
-    # '.md': 'markdown',
-}
+source_suffix = [".rst", ".md"]
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
