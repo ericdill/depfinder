63106c56e3977b0fd7e3dc249542813d88b33c99
Eric Dill <vcs@ericdill.dev>
2023-01-04 12:24:06 -0500

    bad version checking

--- depfinder/stdliblist.py
+++ depfinder/stdliblist.py
@@ -2,13 +2,14 @@ import sys
 import logging
 
 logger = logging.getLogger('depfinder')
+MAJOR, MINOR = sys.version_info.major, sys.version_info.minor
 
-if sys.version_info.major >= 10:
+if MAJOR == 3 and MINOR >= 10:
     builtin_modules = list(set(list(sys.stdlib_module_names) + list(sys.builtin_module_names)))
 else:
     try:
         from stdlib_list import stdlib_list
-        pyver = '%s.%s' % (sys.version_info.major, sys.version_info.minor)
+        pyver = '%s.%s' % (MAJOR, MINOR)
         builtin_modules = stdlib_list(pyver)
         del pyver
     except ImportError:
