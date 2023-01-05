99a635e61a2701c834d1dd3884282d7a93df19fc
ericdill <ericdill@users.noreply.github.com>
2023-01-05 21:33:07 +0000

    deploy: 377f828dfce94b37db984da5b0308a8271da2581

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.9.2.dev12+ge46d069',
+    VERSION: '2.9.2.dev20+g377f828',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
